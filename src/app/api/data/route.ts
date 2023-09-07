9bcfda4fff185a56017f40b744209311be98d846
piyush.kumar05@nagarro.com <piyushkumar05@nagarro.com>
2023-09-07 09:46:42 +0530

    updated package.json

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -3,9 +3,6 @@ import { NextResponse } from 'next/server';
 
 export const GET = async (req:Request,res:Response) => {
   try{
-    
-    
-
     const hostAPIUrl :string = "https://"+process.env.VERCEL_URL+"/" || "http://localhost:3000/"
     return NextResponse.json({url:hostAPIUrl,...process.env},{status:200});
   }catch(err){
